71be15fd7451ed8f0728e9c1bd135e0b64d3db9d
sebastian nor <sebanor03@gmail.com>
2023-04-26 01:01:12 -0300

    tema3

---
+++ 1ertema.java
@@ -0,0 +1,12 @@
+package com.openbootcamp;
+
+public class main{
+    public static void main(String[]args){
+        suma(a=10, b=30, c=20);
+    }
+    public static void suma(int a, int b, int c){
+        int resultado;
+        resultado= a + b + c;
+        System.out.println(resultado);
+    }
+}
